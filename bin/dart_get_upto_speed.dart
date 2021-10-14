aa8e4ef105ba022be83e33d49e8964dc295d7e87
MrNewaz <saifur.saif@northsouth.edu>
2021-10-15 00:04:51 +0600

    string value

--- bin/dart_get_upto_speed.dart
+++ bin/dart_get_upto_speed.dart
@@ -7,18 +7,14 @@ void main(List<String> arguments) {
   print(intNum.toString());
   print(intNum.isEven);
 
-  double doubleNum = 5.5;
-
-  num myNum = 5;
-
-  bool myBool = false;
-
-  dynamic mySomething = 5;
-
-  mySomething = 3;
-
   final x = {'x': 5, 'y': 6};
+  print(x);
 
   String? myString1 = null;
   print(myString1?.length);
+
+  int result = 5 ~/ 5;
+  print(++result);
+
+  print('Hello $result ${5 + 5 + 6}');
 }
