1489726e5e7db9c4f96109434aced8086464a08f
MrNewaz <saifur.saif@northsouth.edu>
2021-10-14 21:03:53 +0600

    variables

--- bin/dart_get_upto_speed.dart
+++ bin/dart_get_upto_speed.dart
@@ -1,3 +1,21 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  final myString = 'Hello world!';
+  print(myString);
+  print(myString.contains('Hello'));
+
+  int intNum = 5;
+  print(intNum.toString());
+  print(intNum.isEven);
+
+  double doubleNum = 5.5;
+
+  num myNum = 5;
+
+  bool myBool = false;
+
+  dynamic mySomething = 5;
+
+  mySomething = 3;
+
+  final x = {'x': 5, 'y': 6};
 }
