d9f7d95d915460460c52c47c5caff71b1c84a8ba
MrNewaz <saifur.saif@northsouth.edu>
2021-10-15 20:05:45 +0600

    hmm

--- bin/dart_get_upto_speed.dart
+++ bin/dart_get_upto_speed.dart
@@ -1,50 +1,18 @@
 void main(List<String> arguments) {
-  final myInt = 5;
-  if (myInt == 5) {
-    print(myInt);
-  }
+  bool isSignedIn = true;
+  <String>[
+    'Fake text',
+    if (isSignedIn) 'Sign Out' else 'Sign in',
+  ];
 
-  for (int i = 0; i < 10; i++) {
-    print(i);
-  }
+  final x = <String>[for (int i = 0; i < 5; i++) i.toString()];
 
-  print(returnString());
+  print(x);
 
-  print(params(5, 4, 'Shetaii'));
+  final list1 = [1, 2];
+  final list2 = [3, 4];
 
-  // final myString = 'Hello world!';
-  // print(myString);
-  // print(myString.contains('Hello'));
+  final list3 = [...list1, ...list2];
 
-  // int intNum = 5;
-  // print(intNum.toString());
-  // print(intNum.isEven);
-
-  // final x = {'x': 5, 'y': 6};
-  // print(x);
-
-  // String? myString1 = null;
-  // print(myString1?.length);
-
-  // int result = 5 ~/ 5;
-  // print(++result);
-
-  // print('Hello $result ${5 + 5 + 6}');
-}
-
-String returnString() {
-  return 'Hmmm';
-}
-
-int params(int x, int y, [String h = 'Hmm']) {
-  print(h);
-  return x + y;
-}
-
-int namedParams({int? x, required int y}) {
-  return y;
-
-  final myList = [1, 2, 3, 'Hello'];
-
-  final mySet = {1, 2, 3};
+  print(list3);
 }
