8258decbaedcb1ec56614c60d5d3bfc2e9cce787
MrNewaz <saifur.saif@northsouth.edu>
2021-10-15 18:25:03 +0600

    collections

--- bin/dart_get_upto_speed.dart
+++ bin/dart_get_upto_speed.dart
@@ -1,20 +1,50 @@
 void main(List<String> arguments) {
-  final myString = 'Hello world!';
-  print(myString);
-  print(myString.contains('Hello'));
+  final myInt = 5;
+  if (myInt == 5) {
+    print(myInt);
+  }
 
-  int intNum = 5;
-  print(intNum.toString());
-  print(intNum.isEven);
+  for (int i = 0; i < 10; i++) {
+    print(i);
+  }
 
-  final x = {'x': 5, 'y': 6};
-  print(x);
+  print(returnString());
 
-  String? myString1 = null;
-  print(myString1?.length);
+  print(params(5, 4, 'Shetaii'));
 
-  int result = 5 ~/ 5;
-  print(++result);
+  // final myString = 'Hello world!';
+  // print(myString);
+  // print(myString.contains('Hello'));
 
-  print('Hello $result ${5 + 5 + 6}');
+  // int intNum = 5;
+  // print(intNum.toString());
+  // print(intNum.isEven);
+
+  // final x = {'x': 5, 'y': 6};
+  // print(x);
+
+  // String? myString1 = null;
+  // print(myString1?.length);
+
+  // int result = 5 ~/ 5;
+  // print(++result);
+
+  // print('Hello $result ${5 + 5 + 6}');
+}
+
+String returnString() {
+  return 'Hmmm';
+}
+
+int params(int x, int y, [String h = 'Hmm']) {
+  print(h);
+  return x + y;
+}
+
+int namedParams({int? x, required int y}) {
+  return y;
+
+  final myList = [1, 2, 3, 'Hello'];
+
+  final mySet = {1, 2, 3};
 }
