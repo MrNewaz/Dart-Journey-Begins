fbaf5bdfa39d3b110dec389c2d1829b0e847415c
MrNewaz <saifur.saif@northsouth.edu>
2021-10-16 00:18:47 +0600

    another file added

---
+++ bin/private_packages.dart
@@ -0,0 +1,10 @@
+import 'dart_get_upto_speed.dart';
+
+void main(List<String> arguments) {
+  User myUser = const User(name: 'Saif', photo: 'pic');
+
+  print({
+    myUser.name,
+    myUser.photo,
+  });
+}
